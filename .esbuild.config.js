1a6a20992db586f945fd3884c92d7e10bda22804
pynickle <2330458484@qq.com>
2025-10-13 21:27:33 +0800

    chore: 加入对 yml 的支持

---
+++ .esbuild.config.js
@@ -0,0 +1,22 @@
+﻿import { build } from 'esbuild'
+import yamlPlugin from 'esbuild-plugin-yaml'
+
+await build({
+    entryPoints: ['src/index.ts'],
+    bundle: true,
+    outfile: 'lib/index.js',
+    format: 'cjs',
+    platform: 'node',
+    external: [
+        'koishi',
+        'axios',
+        'dayjs',
+        'koishi-plugin-cron',
+        'lunar-typescript',
+        'node-emoji',
+        'whoiser',
+        'koishi-plugin-puppeteer',
+        '@koishijs/plugin-adapter-onebot',
+    ],
+    plugins: [yamlPlugin()],
+})
